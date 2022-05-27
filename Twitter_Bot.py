cc1a7b01cccd7daa9f42d35e476af07daaaf1048
th-araujo <th.araujo@outlook.com>
2022-05-27 13:35:23 -0300

    "Teste de credenciais"

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -16,3 +16,21 @@ import tweepy
 # Autenticar no Twitter
 auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
 auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Teste de credenciais Twitter
+
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("pGBDoAaEpkliVKBOLwjtcmHGc",
+    "xF3g1wrP50b6BlZEd20u4oVfjgH1FGQcuWUzlQO5aUWOufvlhw")
+auth.set_access_token("622518493-6VcLIPprbQbv9wkcBBPvCle8vsjU9fE85Dq9oStl",
+    "tH9aKQbQQ1iRdYTcLSsPwitl44BkAc6jilrsU0ifnXvZhq")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
