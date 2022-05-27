5c0bfdab80e7e9c4895cb3f09411463492ad65e0
th-araujo <th.araujo@outlook.com>
2022-05-26 21:09:38 -0300

    "Autenticar"

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -9,3 +9,10 @@ api = tweepy.API(auth)
 
 # Criar um tweet
 api.update_status("Vai Corinthians")
+
+
+import tweepy
+
+# Autenticar no Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
