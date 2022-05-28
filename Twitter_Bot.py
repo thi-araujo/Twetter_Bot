bcf5d992c666fcf4af8ccc9e31a41e77c2589811
th-araujo <th.araujo@outlook.com>
2022-05-28 15:32:37 -0300

    "credenciais"

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -34,3 +34,15 @@ try:
     print("Authentication OK")
 except:
     print("Error during authentication")
+
+
+
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
