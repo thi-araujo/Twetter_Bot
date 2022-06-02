da7a9e3baa85bfc3b4af5408b7be9beb3f0009f3
th-araujo <th.araujo@outlook.com>
2022-06-02 15:57:20 -0300

    "Método para bloquear e desbloquear usuários"

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -72,3 +72,8 @@ tweets = api.home_timeline(count=1)
 tweet = tweets[0]
 print(f"Liking tweet {tweet.id} of {tweet.author.name}")
 api.create_favorite(tweet.id)
+
+# Método para bloquear e desbloquear usuáriso
+
+for block in api.blocks():
+    print(block.name)
