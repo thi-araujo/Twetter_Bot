ddb9e08d56544fae92876b90a18d685542d788a2
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-06-19 23:21:49 -0300

    "autenticar"

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -19,9 +19,9 @@ api.update_status("Meu primeiro tweet")
 api.update_status("Hello World")
 
 
+# Autenticar no Twitter
 import tweepy
 
-# Autenticar no Twitter
 auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
 auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
 
